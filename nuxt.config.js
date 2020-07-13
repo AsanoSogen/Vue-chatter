9b960b52ac854f0c3a56d3ddf6f35e0e41b66eb4
AsanoSogen <AsanoSogen>
2020-07-14 00:55:37 +0900

    ログインチェック機能実装

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,8 @@ export default {
   ** Plugins to load before mounting the App
   ** https://nuxtjs.org/guide/plugins
   */
-  plugins: ['~/plugins/firebase'
+  plugins: ['~/plugins/firebase',
+            '~/plugins/auth',
   ],
   /*
   ** Auto import components
